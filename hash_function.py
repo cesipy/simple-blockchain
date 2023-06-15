1fd2739e2fb4bfb800cbe4e626af1700a8cdb293
cedric <cedric.sillaber@gmail.com>
2023-06-15 19:42:11 +0200

    fix missing brackets in hash_function

--- hash_function.py
+++ hash_function.py
@@ -2,7 +2,7 @@ import block, hashlib
 
 def calculate_hash(block):
     meta_data = f"{block.hash} plus {block.timestamp} plus {block.nonce}"
-    h = hashlib.blake2b     #create hash object
-    h.update(meta_data)
+    h = hashlib.blake2b()     #create hash object
+    h.update(meta_data.encode())
     return h.hexdigest()
 
