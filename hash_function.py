6a2a0c35e2d8622b4c873ef314a638e1ecbba26e
cedric <cedric.sillaber@gmail.com>
2023-06-15 19:37:36 +0200

    implement simple hash_function

---
+++ hash_function.py
@@ -0,0 +1,8 @@
+import block, hashlib
+
+def calculate_hash(block):
+    meta_data = f"{block.hash} plus {block.timestamp} plus {block.nonce}"
+    h = hashlib.blake2b     #create hash object
+    h.update(meta_data)
+    return h.hexdigest()
+
