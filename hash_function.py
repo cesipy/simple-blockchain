e53061d7a10cfe0a2cab5878d0f8ccb755bee1c0
cedric <cedric.sillaber@gmail.com>
2023-06-15 19:49:24 +0200

    add nonce separatly from block

--- hash_function.py
+++ hash_function.py
@@ -1,7 +1,7 @@
 import block, hashlib
 
-def calculate_hash(block):
-    meta_data = f"{block.hash} plus {block.timestamp} plus {block.nonce}"
+def calculate_hash(block, nonce):
+    meta_data = f"{block.hash} plus {block.timestamp} plus {nonce}"
     h = hashlib.blake2b()     #create hash object
     h.update(meta_data.encode())
     return h.hexdigest()
