3b02089b4602007a272a9627645b224769ab2d0d
cedric <cedric.sillaber@gmail.com>
2023-06-15 20:03:15 +0200

    add functionality of creating genesis block and simple proof of work

--- block.py
+++ block.py
@@ -1,20 +1,35 @@
 import time
+import hash_function
 
 class Block:
-    def __init__(self, prev_hash,  nonce, hash, transactions):
+    def __init__(self, prev_hash,  nonce, hash, transactions, difficulty):
         self.prev_hash = prev_hash
         self.timestamp = time.time()
         self.nonce = nonce
         self.hash = hash
         self.transactions = transactions
+        self.difficulty = difficulty
 
     def __repr__(self):
-        return "Block : %s from %s", self.hash, self.timestamp
+        return f"Block : {self.hash} from {self.timestamp}"
     
 
-
-def create_first_block():
-    
-    block =  Block("0", 0, "hash", [])
-    print(block)
+def create_first_block(difficulty):
+    block =  Block("0", 0, "hash", [], difficulty)
     return block
+
+
+def proof_of_work(block):
+    difficulty = block.difficulty       
+    current_nonce = block.nonce
+
+    trying_hash = hash_function.calculate_hash(block, current_nonce)    # is first computed hash a valid hash
+    difficulty_compare = '0' * difficulty                         # essentially '00...' - amount of leading zeros
+
+    # use bruteforce to find valid hash
+    while (trying_hash[0:difficulty] != difficulty_compare):
+        current_nonce+= 1
+        trying_hash = hash_function.calculate_hash(block, current_nonce)
+
+    return trying_hash
+    
--- testing.py
+++ testing.py
@@ -1,8 +1,12 @@
 import block
 
+DIFF = 3
 
 def main():
 
     # generate genisis block
-    genisis_block = block.create_first_block()
-    print(genisis_block)
+    genisis_block = block.create_first_block(DIFF)
+    block.proof_of_work(genisis_block)
+
+
+main()
