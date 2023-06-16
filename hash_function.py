aa94a0dfe00960ed342d45f84f617596565cb10d
cedric <cedric.sillaber@gmail.com>
2023-06-16 22:13:57 +0200

    change hash function to create current hash with previous hash

--- hash_function.py
+++ hash_function.py
@@ -1,7 +1,7 @@
 import block, hashlib, time
 
 def calculate_hash(block, nonce):
-    meta_data = f"{block.hash} plus {block.timestamp} plus {nonce}"
+    meta_data = f"{block.prev_hash} plus {block.timestamp} plus {nonce}"
     h = hashlib.blake2b()     #create hash object
     h.update(meta_data.encode())
     time.sleep(0.001)
