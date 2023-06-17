821a09d412fe7e3c06ccf0b628651a7cba11d39b
cedric <cedric.sillaber@gmail.com>
2023-06-17 11:56:52 +0200

    add todo points

--- todo.py
+++ todo.py
@@ -1,8 +1,25 @@
 '''
     TODO:
         - add block number in metadata
-        - transactions simulation
+
+        - transactions 
+            - class of transactions
+            - random transactions simulator
+            - add transactions at block creation
+            - validate transactions
+            
+
         - add a public ledger that more 'miners' can work on the project
-        
+            - create a Blockchain class
+            - make a method to save to file
+
+        - add wallets
+
+        - network simulation
+            - create mutliple nodes. Multiple Blockchain instances in different threads
+            - when node creates transaction -> broadcast to all other nodes (pending transa. pool)
+            - when node mines new block -> broadcast it ; important to handle any conflicts
+
+
     
 '''
