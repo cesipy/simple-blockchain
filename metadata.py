5d2dfc924ace63d2ba370258af58240b44b70684
cedric <cedric.sillaber@gmail.com>
2023-06-22 10:50:50 +0200

    refractor files

--- metadata.py
+++ metadata.py
@@ -1,16 +1,14 @@
-class Metadata: 
+class Metadata:
     def __init__(self, difficulty, version, reward, block_number):
         self.difficulty = difficulty
         self.version = version
         self.reward = reward
-        self.counter_adjusting_difficulity = 0
+        self.counter_adjusting_difficulty = 0
         self.block_number = block_number
 
-
     def increase_difficulty(self):
-        self.difficulty+= 1
-        self.counter_adjusting_difficulity = 0
+        self.difficulty += 1
+        self.counter_adjusting_difficulty = 0
 
     def get_params(self):
         return self.difficulty, self.version, self.reward, self.block_number
-    
