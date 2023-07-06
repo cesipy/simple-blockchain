c9bcd58e8c1b4a8f0833eb8070db29f9e518d670
cedric <cedric.sillaber@gmail.com>
2023-07-06 09:54:47 +0200

    extract hashfunction to separate file

---
+++ hash_function.py
@@ -0,0 +1,11 @@
+import hashlib
+import time
+
+
+def calculate_hash(current_block) -> str:
+    meta_data = f"{current_block.prev_hash} plus {current_block.timestamp} plus {current_block.nonce}"
+    h = hashlib.blake2b()  # create hash object
+    h.update(meta_data.encode())
+    time.sleep(0.001)
+    return h.hexdigest()
+
